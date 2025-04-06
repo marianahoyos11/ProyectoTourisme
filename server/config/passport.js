15d4193eeffef63b20c755e95d7b6f5b5ce26398
Mariana Hoyos <hoyosmariana62@gmail.com>
2025-04-06 14:53:37 -0500

    file de conexión al servidor para autenticación con google

---
+++ server/config/passport.js
@@ -0,0 +1,53 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require('passport');
+const db = require('../database/database.js');
+
+module.exports = function(passport) {
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback'
+    },
+    async function(accessToken, refreshToken, profile, done) {
+        try {
+            const email = profile.emails[0].value;
+            const fullName = profile.displayName;
+
+            db.query('SELECT * FROM usuarios WHERE email = ?', [email], (err, results) => {
+                if (err) return done(err);
+
+                if (results.length > 0) {
+                    // Usuario ya existe
+                    return done(null, results[0]);
+                } else {
+                    // Crear nuevo usuario
+                    const nuevoUsuario = {
+                        nombre_completo: fullName,
+                        email: email,
+                        password: null,
+                        fecha_registro: new Date()
+                    };
+
+                    db.query('INSERT INTO usuarios SET ?', nuevoUsuario, (err, res) => {
+                        if (err) return done(err);
+                        nuevoUsuario.id = res.insertId;
+                        return done(null, nuevoUsuario);
+                    });
+                }
+            });
+        } catch (error) {
+            return done(error);
+        }
+    }));
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        db.query('SELECT * FROM usuarios WHERE id = ?', [id], (err, results) => {
+            if (err) return done(err);
+            done(null, results[0]);
+        });
+    });
+};
